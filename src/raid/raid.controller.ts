8bb8119c275460cd73599272808c0976679d5208
Park shinyeong <syngh503@gmail.com>
2022-07-12 13:02:20 +0900

    ADD / 보스 레이드 시작 성공 구현
    보스 레이드 시작 시 성공했을 때를 구현하였습니다.
    
    Relate to: #10

--- src/raid/raid.controller.ts
+++ src/raid/raid.controller.ts
@@ -1,27 +1,54 @@
 import {
+  Body,
   CACHE_MANAGER,
   Controller,
   Get,
   Inject,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import { RaidService } from './raid.service';
+import { CreateRaidDTO } from './dto/createRaid.dto';
+import { EnterBossRaidOption } from 'src/common/enterBossOption.interface';
+import { GetUser } from 'src/common/getUserDecorator';
+import { User } from 'src/user/entities/user.entity';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/passport/guard/jwtAuthGuard';
+import { MSG } from 'src/common/response.enum';
 
+@ApiBearerAuth('access_token')
+@UseGuards(JwtAuthGuard)
+@ApiTags('BossRaid')
 @Controller('bossRaid')
 export class RaidController {
   constructor(
     private readonly raidService: RaidService,
     @Inject(CACHE_MANAGER)
-    cacheManager: Cache,
+    private readonly cacheManager: Cache,
   ) {}
 
   @Get()
   getRaid() {}
 
+  @ApiBody({ type: CreateRaidDTO })
+  @ApiCreatedResponse({
+    description: MSG.enterBossRaid.msg,
+  })
   @Post('enter')
-  enterRaid() {}
+  async enterRaid(@Body() createRaidDto: CreateRaidDTO, @GetUser() user: User) {
+    const result = await this.raidService.enterBossRaid(createRaidDto, user);
+
+    // 캐시에 항목 추가
+    await this.cacheManager.set('raidRecord', result, { ttl: 180 });
+    return this.cacheManager.get('raidRecord');
+  }
 
   @Patch('end')
   endRaid() {}
--- src/raid/raid.service.ts
+++ src/raid/raid.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { RaidRecord } from './entities/raid.entity';
+import { CreateRaidDTO } from './dto/createRaid.dto';
+import { EnterBossRaidOption } from 'src/common/enterBossOption.interface';
 
 @Injectable()
 export class RaidService {
@@ -10,6 +12,31 @@ export class RaidService {
     @InjectRepository(RaidRecord)
     private readonly raidRecordRepositroy: Repository<RaidRecord>,
     @InjectRepository(User)
-    private readonly userRepositroy: Repository<User>,
+    private readonly userRepository: Repository<User>,
   ) {}
+
+  async enterBossRaid(
+    createRaidDto: CreateRaidDTO,
+    user: User,
+  ): Promise<EnterBossRaidOption> {
+    // 레이드 상태 조회
+    // 레이드 시작 불가능
+
+    // 레이드 시작 가능
+    try {
+      const newBossRaid = this.raidRecordRepositroy.create({
+        ...createRaidDto,
+        userId: user.id,
+        score: 0,
+      });
+      const result = await this.raidRecordRepositroy.insert(newBossRaid);
+      const enterOption: EnterBossRaidOption = {
+        isEntered: true,
+        raidRecordId: result.identifiers[0].id,
+      };
+      return enterOption;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
