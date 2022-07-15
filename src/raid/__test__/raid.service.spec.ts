9c2ef2780720a31725bfc974da99ef899669c077
LeoKim <leo3179@naver.com>
2022-07-15 12:21:45 +0900

    MODIFY / 레이드 서비스 유닛 테스트 코드 초안 작성
    - 초안 작성 완료
    - 모듈에 해당하는 value를 임의로 지정해놨습니다
    - redis,Bull 등과 연관이 깊고 test코드를 짜기 어렵다면 생략하는게 좋겠습니다
    - Queue는 배열로 처리했습니다
    
    Resolve: #45

--- src/raid/__test__/raid.service.spec.ts
+++ src/raid/__test__/raid.service.spec.ts
@@ -3,7 +3,16 @@
       - raid.service 유닛 테스트 파일 작성
   */
 
-import { Repository } from 'typeorm';
+import { CacheModule, CACHE_MANAGER } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { User } from '@sentry/node';
+import { Queue } from 'bullmq';
+
+import { RedisModule } from 'nestjs-redis';
+import { UserService } from 'src/user/user.service';
+import { Connection, DataSource, QueryBuilder, QueryRunner, Repository } from 'typeorm';
+import { RaidRecord } from '../entities/raid.entity';
+import { RaidService } from '../raid.service';
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
@@ -30,4 +39,78 @@ const mockRepository = () => ({
   }),
 });
 
-describe('RaidService', () => {});
+const mockUserService = () => ({});
+
+describe('RaidService', () => {
+  let raidService: RaidService;
+  let raidRecordRepository: MockRepository<RaidRecord>;
+  let userRepository: MockRepository<User>;
+  let dataSource: DataSource;
+  let cacheModule: CacheModule;
+
+  const qb = {
+    connection: {},
+  } as QueryBuilder<RaidRecord>;
+
+  const qr = {
+    manager: {},
+  } as QueryRunner;
+
+  class MockDataSource {
+    createQueryRunner(): QueryRunner {
+      return qr;
+    }
+
+    createQueryBuilder(): QueryBuilder<RaidRecord> {
+      return qb;
+    }
+  }
+
+  qr.connect = jest.fn();
+  qr.startTransaction = jest.fn();
+  qr.commitTransaction = jest.fn();
+  qr.rollbackTransaction = jest.fn();
+  qr.release = jest.fn();
+
+  beforeAll(async () => {
+    Object.assign(qr.manager, { save: jest.fn() });
+
+    const module = await Test.createTestingModule({
+      providers: [
+        RedisModule,
+        CacheModule,
+        RaidService,
+        {
+          provide: 'RaidRecordRepository',
+          useFactory: mockRepository,
+        },
+        {
+          provide: 'UserRepository',
+          useFactory: mockRepository,
+        },
+        {
+          provide: UserService,
+          useFactory: mockUserService,
+        },
+        { provide: DataSource, useClass: MockDataSource },
+        {
+          provide: CACHE_MANAGER,
+          useValue: CACHE_MANAGER,
+        },
+        { provide: 'BullQueue_playerQueue', useValue: [] },
+        { provide: 'default_IORedisModuleConnectionToken', useExisting: RedisModule },
+        { provide: Connection, useClass: MockDataSource },
+      ],
+    }).compile();
+
+    raidService = module.get<RaidService>(RaidService);
+    raidRecordRepository = module.get('RaidRecordRepository') as MockRepository<RaidRecord>;
+    userRepository = module.get('UserRepository') as MockRepository<User>;
+    dataSource = module.get<DataSource>(DataSource);
+    cacheModule = module.get<CacheModule>(CacheModule);
+  });
+
+  it('isDefined 테스트', () => {
+    expect(raidService).toBeDefined();
+  });
+});
