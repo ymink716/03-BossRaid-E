93ca01da59cf2a15d3ef39c21803b720dde48822
scvgood287 <scvgood0763@gmail.com>
2022-07-15 13:38:55 +0900

    FIX / EB Deploy Test 17

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,6 @@ import type { RedisClientOptions } from 'redis';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: [`env/.env`],
     }),
     UserModule,
     AuthModule,
