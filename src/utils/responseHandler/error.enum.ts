75f253844b276c8474d41e49654381adcce05335
LeoKim <leo3179@naver.com>
2022-07-16 11:58:10 +0900

    REFACTOR / 스웨거 RES 관련 명세 생성
    - 레이드 상태 조회 api 스웨거 RES 명세 작성
    - 레이드 전역에 unauthorized res 추가
    
    Relate to: [Task] 스웨거 리스폰스 타입 지정, example, description #89

--- src/utils/responseHandler/error.enum.ts
+++ src/utils/responseHandler/error.enum.ts
@@ -1,4 +1,7 @@
 export const ErrorType = {
+  // Auth
+  unAuthorized: { code: 401, msg: '로그인 후 이용해주세요' },
+
   // User
   invalideUser: { code: 400, msg: '유효한 사용자가 아닙니다!' },
   userExists: { code: 400, msg: '이미 존재하는 유저입니다!' },
@@ -7,14 +10,12 @@ export const ErrorType = {
   nicknameExist: { code: 409, msg: '해당 닉네임은 이미 사용중입니다!' },
   emailExist: { code: 409, msg: '해당 이메일은 이미 사용중입니다!' },
 
-
   // RaidRecord
   raidRecordNotFound: { code: 404, msg: '레이드 기록이 존재하지 않습니다' },
-  raidStatusNotFound: { code: 404, msg: '진행 중인 레이드가 없습니다.'},
-  raidStatusBadRequest: { code: 400, msg: '진행 중인 레이드 정보와 일치하지 않습니다.'},
-  raidStatusForbidden: { code: 400, msg: '레이드에 접근할 수 없습니다.'},// 추후 수정
-  raidLevelNotFound: { code: 404, msg: '해당 레이드 레벨이 존재하지 않습니다.'},
-
+  raidStatusNotFound: { code: 404, msg: '진행 중인 레이드가 없습니다.' },
+  raidStatusBadRequest: { code: 400, msg: '진행 중인 레이드 정보와 일치하지 않습니다.' },
+  raidStatusForbidden: { code: 400, msg: '레이드에 접근할 수 없습니다.' }, // 추후 수정
+  raidLevelNotFound: { code: 404, msg: '해당 레이드 레벨이 존재하지 않습니다.' },
 
   // Server
   serverError: { code: 500, msg: '서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.' },
@@ -23,5 +24,5 @@ export const ErrorType = {
 
   // Utils
   axiosError: { code: 421, msg: '레이드 정보 json을 불러오지 못했습니다' },
-  bullError:{ code: 421, msg: '큐에 정보를 저장하지 못했습니다.'} // 추후 수정
+  bullError: { code: 421, msg: '큐에 정보를 저장하지 못했습니다.' }, // 추후 수정
 };
